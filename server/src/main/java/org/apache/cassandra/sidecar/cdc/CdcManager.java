3b5fc7659df00dca3b26c481db0b3849a02ca749
Bernardo Botella Corbi <contacto@bernardobotella.com>
2026-01-07 19:16:57 +0100

    Fix tests

--- server/src/main/java/org/apache/cassandra/sidecar/cdc/CdcManager.java
+++ server/src/main/java/org/apache/cassandra/sidecar/cdc/CdcManager.java
@@ -26,6 +26,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -162,7 +163,8 @@ public class CdcManager
                                         throw new RuntimeException(e);
                                     }
                                 });
-                            }));
+                            }))
+                   .collect(Collectors.toList());;
 
         consumers = new ArrayList<>(uniqueConsumers.values());
         return consumers;
